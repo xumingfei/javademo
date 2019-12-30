7304a4cbf58a7454aa3ab64daa808cb0cfdb3c1e
Mr.Xu <18832892030@163.com>
2019-12-31 07:09:52 +0800

    增加学习网址

---
+++ practice/src/com/practice/numTest.java
@@ -0,0 +1,12 @@
+package com.practice;
+
+public class numTest {
+    public static void main(String[] args) {
+        byte a = 20; // 声明一个byte类型的变量并赋予初始值为20
+        short b = 10; // 声明一个short类型的变量并赋予初始值为10
+        int c = 30; // 声明一个int类型的变量并赋予初始值为30
+        long d = 40; // 声明一个long类型的变量并赋予初始值为40
+        long sum = a + b + c + d;
+        System.out.println("20+10+30+40=" + sum);
+    }
+}
