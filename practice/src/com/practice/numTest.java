9ad523b0515603de0fc83dc8f491b2308b39b521
Mr.Xu <18832892030@163.com>
2019-12-31 07:13:01 +0800

    test

--- practice/src/com/practice/numTest.java
+++ practice/src/com/practice/numTest.java
@@ -7,6 +7,6 @@ public class numTest {
         int c = 30; // 声明一个int类型的变量并赋予初始值为30
         long d = 40; // 声明一个long类型的变量并赋予初始值为40
         long sum = a + b + c + d;
-        System.out.println("20+10+30+40=" + sum);
+        System.out.println("20+10+30+40=" + sum);//
     }
 }
